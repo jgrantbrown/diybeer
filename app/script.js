ddc79818c0e9030cee40600806813888d2c127bb
Your Name <you@example.com>
2017-03-28 15:23:03 -0400

    render li

--- app/script.js
+++ app/script.js
@@ -2,16 +2,17 @@
 var url = "https://api.punkapi.com/v2/beers"
 // function searchRepositories
 
-  $(document).ready(function(){
-    // Now we start the Ajax GET request. The first parameter is the URL with the data.
-    // The second parameter is a function that handles the response.
-    $.get(url, function(response) {
-      // Here we are getting the element on the page with the id of sentences and
-      // inserting the response
-      $("#list").text(response.forEach(function(entry) {
-      console.log(entry.name)
-      })
-)
-
-    });
-  })
+$(document).ready(function(){
+  // Now we start the Ajax GET request. The first parameter is the URL with the data.
+  // The second parameter is a function that handles the response.
+  $.get(url, function(response) {
+    // Here we are getting the element on the page with the id of sentences and
+    // inserting the response
+    let html = []
+    response.forEach((entry) => {
+      html.push(`<li>${entry.name}</li>`)
+    })
+    
+    $("#list").append(html.join(""))
+  });
+})
