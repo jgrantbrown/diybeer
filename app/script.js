2e7950af81e3afa40f0ef614e5126b2e2d52a6dd
Alexandra Roth <alroth8@gmail.com>
2017-03-29 18:19:52 -0400

    drrr

--- app/script.js
+++ app/script.js
@@ -9,25 +9,31 @@ $(document).ready(function(){
     
    
     var data = response.map((entry) => {
-    return (`<div class="beer" id="${entry.id}" data-name="${entry.name}" data-tagline="${entry.tagline}">
-      <img src=${entry.image_url} height=75% width=25%></div>`)
 
+    return (`<div class="beer" id="${entry.id}" data-name="${entry.name}" data-tagline="${entry.tagline}">
+    			<img onmouseover="beerPopUp(this)" src=${entry.image_url} height=75% width=25%>
+    		</div>
+    		<div class="beer popup" id="${entry.id}" data-name="${entry.name}" data-tagline="${entry.tagline}">
+    	 	<span class="popuptext" id="myPopup">
+     			(${entry.name})
+  	   			(${entry.tagline})
+      		</span>
+      		</div>`)
   
     })
        	$("#list").append(data)
-    
-  	   	var attr = $("div.beer").each(function(e){
-  	   		$(this).on("mouseover", function(event) {
-  	   			console.log($(this)[0].attributes[2].value)
-  	   			console.log($(this)[0].attributes[3].value)
-  			})
-
-  	   		$("div.beer").append(attr)
-  	   	
-  			
-  		})
-  	})
+       	
+    })
  })
+
+function beerPopUp(event) {
+	// ("myPopup").classList.toggle("show")
+	var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+			
+}
+
+
   	
   // 	$(`[data-id='${event.target.dataset.id}']`).text(`name: ${event.target.dataset.name}`)
   // }))
@@ -35,6 +41,11 @@ $(document).ready(function(){
   // 	$(`[data-id='${event.target.dataset.id}']`).text(`${event.target.dataset.image_url}`)
   // }))
   // .attributes.2.nodeValue)
+
+  // $(this).on("click", function(event) {
+  // 	   			return (`<div class="popuptext" id="myPopup"><div>($(this)[0].attributes[2].value)
+  // 	   				<br>($(this)[0].attributes[3].value)></div></div>`)
+  // 			})
 
 
 
