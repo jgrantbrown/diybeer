1eeb7e9071de723f245e9d341c2997c17611844e
Alexandra Roth <alroth8@gmail.com>
2017-03-30 15:17:44 -0400

    more, closer

---
+++ app/beer.js
@@ -0,0 +1,10 @@
+class Beer {
+	constructor(id, name, tagline, image_url, abv, food_pairing) {
+		this.id = id
+    	this.name = name
+    	this.tagline = tagline
+    	this.image_url = image_url
+    	this.abv = abv
+    	this.food_pairing = food_pairing
+  }
+}
--- app/script.js
+++ app/script.js
@@ -5,18 +5,17 @@ var url = "https://api.punkapi.com/v2/beers"
 $(document).ready(function(){
  
   $.get(url, function(response) {
-    //make beer
-    //make beer into divs
-
     
-   
-    var data = response.map((entry) => {
+      var data = response.map((entry) => {
 
-    return (`<div class="beer" id="${entry.id}" data-name="${entry.name}" data-tagline="${entry.tagline}">
-    			 <img onmouseover="beerPopUp(event)" src=${entry.image_url} height=75% width=25%>
-    	        <div class="beer popup" id="${entry.id}" data-name="${entry.name}" data-tagline="${entry.tagline}">
-     			      (${entry.name})
-  	   			    (${entry.tagline})
+        var beer = new Beer(entry.id, entry.name, entry.tagline, entry.image_url, entry.abv, entry.food_pairing)
+
+
+    return (`<div class="beer" id="${beer.id}" data-name="${beer.name}" data-tagline="${beer.tagline}">
+    			 <img onmouseover="beerPopUp(event)" onmouseout="beerPopUp(event)" src=${beer.image_url} height=75% width=25%>
+    	        <div class="beer popup" id="${beer.id}" data-name="${beer.name}" data-tagline="${beer.tagline}">
+     			      (${beer.name})
+  	   			    (${beer.tagline})
       		    </div>
              </div>`)
   
