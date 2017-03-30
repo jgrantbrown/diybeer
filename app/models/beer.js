07cf716a84bfadfb75f02028a70c9fe4a5a13b1d
Alexandra Roth <alroth8@gmail.com>
2017-03-30 14:00:12 -0400

    still icky

--- app/models/beer.js
+++ app/models/beer.js
@@ -1,20 +1,27 @@
-var storeBeer = []
-
-class Beer 
-{
-	constructor(name, tagline, image_url, abv, food_pairing) {
-    this.name = name
-    this.tagline = tagline
-    this.image_url = image_url
-    this.abv = abv
-    this.food_pairing = food_pairing
-    storeBeer.push(this)
-  }
-
-  static find(id){
-    return $.getJSON(`"https://api.punkapi.com/v2/beers/${id}`).
-                then((beer) => {
-                  new Beer(beer.name, beer.tagline, beer.image_url, beer.abv, beer.food_pairing)
-                })
+class Beer {
+  static all(){
+    return  $.getJSON(`"https://api.punkapi.com/v2/beers/${id}"`)
   }
 }
+
+
+
+
+
+// class Beer {
+// 	constructor(name, tagline, image_url, abv, food_pairing) {
+//     this.name = name
+//     this.tagline = tagline
+//     this.image_url = image_url
+//     this.abv = abv
+//     this.food_pairing = food_pairing
+//     storeBeer.push(this)
+//   }
+
+//   static find(id){
+//     return $.getJSON(`"https://api.punkapi.com/v2/beers/${id}`).
+//                 then((beer) => {
+//                   new Beer(beer.name, beer.tagline, beer.image_url, beer.abv, beer.food_pairing)
+//                 })
+//   }
+// }
--- app/script.js
+++ app/script.js
@@ -5,20 +5,20 @@ var url = "https://api.punkapi.com/v2/beers"
 $(document).ready(function(){
  
   $.get(url, function(response) {
+    //make beer
+    //make beer into divs
 
     
    
     var data = response.map((entry) => {
 
     return (`<div class="beer" id="${entry.id}" data-name="${entry.name}" data-tagline="${entry.tagline}">
-    			<img onmouseover="beerPopUp(this)" src=${entry.image_url} height=75% width=25%>
-    		</div>
-    		<div class="beer popup" id="${entry.id}" data-name="${entry.name}" data-tagline="${entry.tagline}">
-    	 	<span class="popuptext" id="myPopup">
-     			(${entry.name})
-  	   			(${entry.tagline})
-      		</span>
-      		</div>`)
+    			 <img onmouseover="beerPopUp(event)" src=${entry.image_url} height=75% width=25%>
+    	        <div class="beer popup" id="${entry.id}" data-name="${entry.name}" data-tagline="${entry.tagline}">
+     			      (${entry.name})
+  	   			    (${entry.tagline})
+      		    </div>
+             </div>`)
   
     })
        	$("#list").append(data)
@@ -27,25 +27,13 @@ $(document).ready(function(){
  })
 
 function beerPopUp(event) {
-	// ("myPopup").classList.toggle("show")
-	var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
+ 
+	var popup = $(event.target).closest("div").children().eq(1)
+  // debugger
+  popup.toggleClass("show");
 			
 }
 
-
-  	
-  // 	$(`[data-id='${event.target.dataset.id}']`).text(`name: ${event.target.dataset.name}`)
-  // }))
-  // .then(document.getElementById("list").addEventListener("mouseout", function(event) {
-  // 	$(`[data-id='${event.target.dataset.id}']`).text(`${event.target.dataset.image_url}`)
-  // }))
-  // .attributes.2.nodeValue)
-
-  // $(this).on("click", function(event) {
-  // 	   			return (`<div class="popuptext" id="myPopup"><div>($(this)[0].attributes[2].value)
-  // 	   				<br>($(this)[0].attributes[3].value)></div></div>`)
-  // 			})
 
 
 
